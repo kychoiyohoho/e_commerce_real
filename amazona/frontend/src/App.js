2e30a234a14bde23f9a614d95720f4e624fcb73d
kychoiyohoho <imjaichoi@gmail.com>
2022-07-06 12:01:57 +0900

    map 이용하고 고유값을 a 태그에 담음

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,39 @@
+import data from "./data";
+
+function App() {
+  return (
+    <div >
+      <header >
+       <a href="/">amazona</a>
+      </header>
+      <main>
+      <h1>Featured Products</h1>
+      <div className="products">
+      {data.products.map(product=>(
+        <div className="product" key={product.slug}>
+          <a href={`/product/${product.slug}`}>
+          <img src={product.image} alt={product.name}/>
+          </a>
+          {/*  */}
+         <div className="product-info">
+         <a href={`/product/${product.slug}`}>
+          <p>
+            {product.name}
+          </p>
+          </a>
+          <a href={`/product/${product.slug}`}>
+          <p><strong>
+            {product.price}
+            </strong>
+          </p>
+          </a>
+          </div>
+        </div>
+      ))}
+      </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
